761f61ae9cce4712d26efb32d5f64e2f57928427
Daniel <nieltansah@gmail.com>
2017-07-28 14:07:30 +0000

    main: Expose available register variables

--- src/main.c
+++ src/main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    unsigned int bit1 : 1;
-    unsigned int bit2 : 1;
-    unsigned int bit3 : 1;
-    unsigned int bit4 : 1;
-    unsigned int bit5 : 1;
-    unsigned int bit6 : 1;
-    unsigned int bit7 : 1;
-    unsigned int bit8 : 1;
-} reg;
+#include "main.h"
 
 reg reg1;
 reg reg2;
