a3d116a33fbeaa250d5ecc65949e867a7a99896e
Daniel <nieltansah@gmail.com>
2017-07-30 17:52:17 +0700

    misc/logic: Add byte manipulation routines

--- src/misc/logic.h
+++ src/misc/logic.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+/* Bit manipulation. */
+
 #define LOGIC_AND(a,b) \
     ((a) ? ((b) ? 1 : 0) : 0)
 
@@ -12,3 +14,21 @@
 
 #define LOGIC_XOR(a,b) \
     LOGIC_AND(!LOGIC_AND(a,b),LOGIC_OR(b,a))
+
+/* Byte manipulation. */
+
+#define _LOGIC_APPLY_UNIT_STMT(rega,regb_const,i,call_stmt) \
+    (rega).bit##i = call_stmt((rega).bit##i,(regb_const).bit##i)
+
+#define _LOGIC_APPLY_STMT(rega,regb_const,call_stmt) \
+    _LOGIC_APPLY_UNIT_STMT(rega,regb_const,1,call_stmt); \
+    _LOGIC_APPLY_UNIT_STMT(rega,regb_const,2,call_stmt); \
+    _LOGIC_APPLY_UNIT_STMT(rega,regb_const,3,call_stmt); \
+    _LOGIC_APPLY_UNIT_STMT(rega,regb_const,4,call_stmt); \
+    _LOGIC_APPLY_UNIT_STMT(rega,regb_const,5,call_stmt); \
+    _LOGIC_APPLY_UNIT_STMT(rega,regb_const,6,call_stmt); \
+    _LOGIC_APPLY_UNIT_STMT(rega,regb_const,7,call_stmt); \
+    _LOGIC_APPLY_UNIT_STMT(rega,regb_const,8,call_stmt)
+
+#define LOGIC_XOR_STMT(rega,regb_const) \
+    _LOGIC_APPLY_STMT(rega,regb_const,LOGIC_XOR)
