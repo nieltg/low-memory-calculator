382c716a96cabb076134c360eb1aff563cbfc4bf
Daniel <nieltansah@gmail.com>
2017-07-30 20:33:13 +0700

    test/test-sub: Add test for substraction routine

---
+++ test/test-sub.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../src/main.h"
+#include "../src/math.h"
+#include "../src/math/add.h"
+#include "../src/math/sub.h"
+#include "../src/misc/util.h"
+
+/* Environment. */
+
+reg reg1;
+reg reg2;
+reg reg3;
+reg reg4;
+
+MATH_IMPL(MATH_SUB,reg1,reg2,reg3);
+
+MATH_IMPL(MATH_ADD,reg1,reg2);
+MATH_IMPL(MATH_ADD,reg2,reg3);
+
+/* Test. */
+
+int main (int argc, char** argv) {
+
+    unsigned int i, j, val;
+
+    for (i = 0; i < 256; i++) {
+        for (j = 0; j < 256; j++) {
+            UTIL_CAST_UCHAR(reg1) = i;
+            UTIL_CAST_UCHAR(reg2) = j;
+
+            val = i - j;
+            MATH_SUB(reg1,reg2,reg3);
+
+            if (UTIL_CAST_UCHAR(reg1) != (unsigned char) val) {
+                fprintf (stderr, "%u - %u = %u, not %u\n", i, j, val,
+                    UTIL_CAST_UCHAR(reg1));
+                return EXIT_FAILURE;
+            }
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
