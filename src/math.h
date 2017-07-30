bd9674b093cb16c901a1d4ef758237c37a55abed
Daniel <nieltansah@gmail.com>
2017-07-30 17:54:08 +0700

    math: Automate method decl. creation

--- src/math.h
+++ src/math.h
@@ -4,21 +4,46 @@
 
 #pragma once
 
-#include "math/add.h"
-#include "math/mul.h"
+#define MATH_IMPL(decl_name,...) \
+    void decl_name(__VA_ARGS__) { \
+        decl_name##_STMT(__VA_ARGS__); \
+    }
+
+#define _MATH_DEFINE1_DECL(decl_name,...) \
+    void decl_name(reg1,##__VA_ARGS__); \
+    void decl_name(reg2,##__VA_ARGS__); \
+    void decl_name(reg3,##__VA_ARGS__); \
+    void decl_name(reg4,##__VA_ARGS__)
+
+#define _MATH_DEFINE2_DECL(decl_name,...) \
+    _MATH_DEFINE1_DECL(decl_name,reg1,##__VA_ARGS__); \
+    _MATH_DEFINE1_DECL(decl_name,reg2,##__VA_ARGS__); \
+    _MATH_DEFINE1_DECL(decl_name,reg3,##__VA_ARGS__); \
+    _MATH_DEFINE1_DECL(decl_name,reg4,##__VA_ARGS__)
+
+#define _MATH_DEFINE3_DECL(decl_name,...) \
+    _MATH_DEFINE2_DECL(decl_name,reg1,##__VA_ARGS__); \
+    _MATH_DEFINE2_DECL(decl_name,reg2,##__VA_ARGS__); \
+    _MATH_DEFINE2_DECL(decl_name,reg3,##__VA_ARGS__); \
+    _MATH_DEFINE2_DECL(decl_name,reg4,##__VA_ARGS__)
 
 /* Add. */
 
 #define MATH_ADD(rega,regb) \
     math_add_##rega##_##regb ()
 
-void MATH_ADD(reg1,reg2);
-void MATH_ADD(reg1,reg3);
+_MATH_DEFINE2_DECL(MATH_ADD);
 
 /* Multiply. */
 
 #define MATH_MUL(rega,regb) \
     math_mul_##rega##_##regb ()
 
-void MATH_MUL(reg1,reg2);
-void MATH_MUL(reg1,reg3);
+_MATH_DEFINE2_DECL(MATH_MUL);
+
+/* Power. */
+
+#define MATH_POW(rega,regb,regt1) \
+    math_pow_##rega##_##regb##_##regt1 ()
+
+_MATH_DEFINE3_DECL(MATH_POW);
