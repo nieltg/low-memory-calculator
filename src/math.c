c48a18cab52e8e3436399d3d2b08d7bd3a63ba45
Daniel <nieltansah@gmail.com>
2017-07-31 14:11:51 +0700

    math: Add needed macro instances

--- src/math.c
+++ src/math.c
@@ -3,25 +3,47 @@
  */
 
 #include "main.h"
+#include "math/add.h"
+#include "math/sub.h"
+#include "math/mul.h"
+#include "math/div.h"
+#include "math/pow.h"
+#include "math/sqrt.h"
 
 #include "math.h"
 
 /* Add. */
 
-void MATH_ADD(reg1,reg2) {
-    MATH_ADD_STMT(reg1,reg2);
-}
+MATH_IMPL(MATH_ADD,reg1,reg1);
+MATH_IMPL(MATH_ADD,reg1,reg2);
+MATH_IMPL(MATH_ADD,reg1,reg3);
+MATH_IMPL(MATH_ADD,reg2,reg2);
+MATH_IMPL(MATH_ADD,reg2,reg3);
+MATH_IMPL(MATH_ADD,reg3,reg3);
+MATH_IMPL(MATH_ADD,reg3,reg4);
 
-void MATH_ADD(reg1,reg3) {
-    MATH_ADD_STMT(reg1,reg3);
-}
+/* Substract. */
+
+MATH_IMPL(MATH_SUB,reg1,reg2,reg3);
 
 /* Multiply. */
 
-void MATH_MUL(reg1,reg2) {
-    MATH_MUL_STMT(reg1,reg2);
-}
+MATH_IMPL(MATH_MUL,reg1,reg2);
+MATH_IMPL(MATH_MUL,reg1,reg3);
+MATH_IMPL(MATH_MUL,reg2,reg3);
+MATH_IMPL(MATH_MUL,reg3,reg4);
 
-void MATH_MUL(reg1,reg3) {
-    MATH_MUL_STMT(reg1,reg3);
-}
+/* Divide. */
+
+MATH_IMPL(MATH_DIV,reg1,reg2,reg3,reg4);
+
+/* Power. */
+
+MATH_IMPL(MATH_POW,reg1,reg3,reg4);
+MATH_IMPL(MATH_POW,reg2,reg3,reg4);
+
+/* Square root. */
+
+MATH_IMPL(MATH_SQRT,reg1);
+MATH_IMPL(MATH_SQRT,reg2);
+MATH_IMPL(MATH_SQRT,reg3);
